ba20f6f9da6de37a961f9ad8d8392ad314977651
shanemoyo <smoyo@pdx.edu>
2020-05-31 14:26:24 -0700

    create respinsive navbar.

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import Router from './Router'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Router/>  
-      </header>
+      <Router/> 
     </div>
   );
 }
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,16 +1,28 @@
 import React from 'react'; 
 import { Link } from "react-router-dom";
+import './Nav.scss';
 
 
 export default function Nav() { 
     return (
-        <nav className="topnav"> 
-            <a>
-                <Link to="/">Home</Link>
-            </a>  
-            <a>
-                <Link to="/about">About</Link>
-            </a>          
+        <nav className="navbar">        
+            <label className="navbar-toggle" id="js-navbar-toggle" for="chkToggle">
+                <span className="hamburger">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </span>
+            </label>
+            <a href="#" className="logo">Shane Moyo</a>
+            <input type="checkbox" id="chkToggle"></input>
+            <ul className="main-nav" id="js-menu">
+                <li>
+                    <Link to="/" className="nav-links">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about" className="nav-links">About</Link>
+                </li>
+            </ul>
         </nav>
     );
 }
