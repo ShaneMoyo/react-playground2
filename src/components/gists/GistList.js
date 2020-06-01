126261c964572cea358ffb372ea63cefd1b8fce8
shanemoyo <smoyo@pdx.edu>
2020-06-01 16:44:35 -0700

    add table styles

--- src/components/gists/GistList.js
+++ src/components/gists/GistList.js
@@ -1,28 +1,38 @@
 import React from 'react'; 
 import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
+import "./GistList.scss"
 
 
 export default function GistList({ gists }) {
     const gistItem = gist => (
-        <li key={gist.id}>
-            <Link to={`/gist/${gist.id}`}>
-                <span>
-                    <p>{gist.description}</p>
-                </span>
-            </Link>
-        </li>
+        <div key={gist.id} className="Table-row">
+            <div class="Table-row-item"><input type="button" /></div>
+            <div class="Table-row-item">{gist.description}</div>
+            <div class="Table-row-item">{gist.id}</div>
+            <div class="Table-row-item"><Link to={`/gist/${gist.id}`}>See more</Link></div>
+        </div>
     );
     
     const gistItems = gists ? gists.map(gist => gistItem(gist)) : null;
 
     return (
-        <ul>
-            {gistItems}
-        </ul>
+        <div className="Table">
+            <div class="Table-row Table-header">
+                <div class="Table-row-item"><input type="button" /></div>
+                <div class="Table-row-item">Description</div>
+                <div class="Table-row-item">ID</div>
+                <div class="Table-row-item">View Details</div>
+            </div>
+            <div class="row-collection">
+                {gistItems}
+            </div>
+        </div>
     )
 }
 
 GistList.propTypes = {
     gists: PropTypes.array
   };
+
+
