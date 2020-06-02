1c48c616d3519a62319002d485b9f8e45ef15852
shanemoyo <smoyo@pdx.edu>
2020-06-02 13:24:14 -0700

    refactor router.

--- src/app/Router.js
+++ src/app/Router.js
@@ -9,6 +9,7 @@ import {
 import Gists from '../components/gists/Gists'; 
 import Gist from '../components/gist/Gist';
 import Nav from '../components/nav/Nav';
+//<Route path={`${match.path}/:subId`} component={Resource} />
 
 
 export default function Router() { 
@@ -17,12 +18,8 @@ export default function Router() {
           <Nav/>
             <div>
                 <Switch>
-                    <Route path="/gist/:id">
-                        <Gist/>
-                    </Route>
-                    <Route path="/">
-                        <Gists/> 
-                    </Route>
+                    <Route path="/gist/:id" component={Gist}/>      
+                    <Route path="/" component={Gists}/>                  
                 </Switch>
             </div>
       </BrowserRouter>
