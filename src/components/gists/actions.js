316791797363bf090ce5e4c8a62b540cca513dcd
shanemoyo <smoyo@pdx.edu>
2020-06-04 09:33:09 -0700

    refactor actions.

--- src/components/gists/actions.js
+++ src/components/gists/actions.js
@@ -3,14 +3,10 @@ import api from '../../services/api';
 
 
 export function fetchGist(username){
-console.log('reducin')
-  return dispatch => {
-    dispatch({ type: actions.LOADING });
+  return dispatch => {   
     dispatch({
       type: actions.LOAD_GISTS,
       payload: api.getGistByUsername(username)
     })
-    .then(() => dispatch({ type: actions.DONE_LOADING}))
-    .catch(error => dispatch({ type: actions.ERROR , payload: error }));
   };
 }
